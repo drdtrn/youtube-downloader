c3c4916c16e3488f5cf57437f894f94e7bfb0586
drdtrn <dardaniiiiiii@gmail.com>
2025-06-02 20:25:57 +0200

    fix minor issues, add custom downloads directory naming prompt

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import yt_dlp
 import os
 
 os.makedirs('./yt_dlp_cache', exist_ok=True)
-os.makedirs('./downloaded', exist_ok=True)
+
+custom_dir = input('\033[1mSet directory name where we should save your downloads: \033[0m').lower()
+if custom_dir:
+    os.makedirs(f'./{custom_dir}', exist_ok=True)
+else:
+    os.makedirs('downloaded', exist_ok=True)
 
 
 ydl_opts = {
@@ -29,7 +34,7 @@ ydl_opts = {
         'outtmpl': f'downloaded/%(title)s.%(ext)s',
         'cachedir': './yt_dlp_cache',
         'verbose': False, #Enable for debugging
-        'quiet': True,
+        'quiet': False,
     }
 
 video_list = []
@@ -52,6 +57,7 @@ while True:
                 print(f'\033[1;32mVideo downloaded successfully to "downloaded" folder.\033[0m')
         except Exception as error:
             print(f'\033[1;31mAn error occurred: {error}!\033[0m')
+        break
     elif list_or_single == 'n':
         print('''\t\t\033[1;33mIf you are downloading only one video answer N when asked if you want to add more links,\n
               If you want to download more than one video, add the links one by 
