4978be0dff09aeb786c0cf8d2d814af35e4042e6
drdtrn <dardaniiiiiii@gmail.com>
2025-06-02 20:44:00 +0200

    bug fixes

--- main.py
+++ main.py
@@ -37,45 +37,44 @@ ydl_opts = {
         'quiet': False,
     }
 
-video_list = []
-
-
-while True:
-    print('''\t\t####################################################################
+print('''\t\t####################################################################
 \n\t\t\033[1;4;31mYouTube downloader created By: \033[1;4;32mDardan Ternava !!!\033[0m\n
 \t\t#################################################################### \n
 \t\t\033[1;33mThis app makes it possible to download whole YouTube video lists, single videos or multiple videos.
 \t\tJust follow the prompts and answer accordingly. \033[0m\n
 ''')
-    list_or_single = input('\t\033[1;4;32mAre you downloading a whole list of YouTube videos ? \033[44;30m(Y/N):\033[0m ').lower()
-    if list_or_single == 'y':
-        user_input = input('\t\033[1;4;31mPaste your link here:\033[0m ')
-        try:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                print('\033[1;33mDownloading...\n This may take a while depending on the connection!\033[0m')
-                ydl.download([user_input])
-                print(f'\033[1;32mVideo downloaded successfully to "downloaded" folder.\033[0m')
-        except Exception as error:
-            print(f'\033[1;31mAn error occurred: {error}!\033[0m')
-        break
-    elif list_or_single == 'n':
-        print('''\t\t\033[1;33mIf you are downloading only one video answer N when asked if you want to add more links,\n
-              If you want to download more than one video, add the links one by 
-              one and answer Y each time you want to add a link to download.\033[0m''')
+list_or_single = input('\t\033[1;4;32mAre you downloading a whole list of YouTube videos ? \033[44;30m(Y/N):\033[0m ').lower()
+if list_or_single == 'y':
+    user_input = input('\t\033[1;4;31mPaste your link here:\033[0m ')
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            print('\033[1;33mDownloading...\n This may take a while depending on the connection!\033[0m')
+            ydl.download([user_input])
+            print(f'\033[1;32mVideo downloaded successfully to "downloaded" folder.\033[0m')
+    except Exception as error:
+        print(f'\033[1;31mAn error occurred: {error}!\033[0m')
+elif list_or_single == 'n':
+    video_list = []
+    print('''\t\t\033[1;33mIf you are downloading only one video answer N when asked if you want to add more links,\n
+          If you want to download more than one video, add the links one by 
+          one and answer Y each time you want to add a link to download.\033[0m''')
+    while True:
+
         user_input = input('\t\033[1;4;31mPaste your link here:\033[0m ')
         confirmation = input('\t\033[1;4;32mDo you want to add more links ? \033[44;30m(Y/N):\033[0m  ').lower()
         if confirmation == 'y':
             video_list.append(user_input)
         elif confirmation == 'n':
+            video_list.append(user_input)
             try:
                 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                     print('\033[1;33mDownloading...\n This may take a while depending on the connection!\033[0m')
-                    ydl.download([user_input])
+                    ydl.download(video_list)
                     print(f'\033[1;32mVideo downloaded successfully to "downloaded" folder.\033[0m')
             except Exception as error:
                 print(f'\033[1;31mAn error occurred: {error}!\033[0m')
             break
         else:
             print('\033[1;31mInvalid input !\033[0m')
-    else:
-        print('\033[1;31mInvalid input !\033[0m')
+else:
+    print('\033[1;31mInvalid input !\033[0m')
